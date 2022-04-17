1a09fad195c663eee7fa98ef62e92f34148c6705
page32burn <page32burn@gmail.com>
2022-04-18 08:48:54 +0900

    変数の指定

--- hello.go
+++ hello.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+var message string = "hello world"
+
+var foo, bar, buz string = "foo", "bar", "buz"
+
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(message)
     fmt.Printf("さようなら")
+    fmt.Printf(foo)
+    fmt.Printf(bar)
+    fmt.Printf(buz)
 }
